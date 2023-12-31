8367bfd865034c947a5c0bdec7782db3db63f6d1
114259-COLLAZO-GABRIELA <128556527+114259-COLLAZO-GABRIELA@users.noreply.github.com>
2023-12-31 03:48:06 -0300

    funciona el alineamiento de las cards

--- src/pages/Profesionals.js
+++ src/pages/Profesionals.js
@@ -24,29 +24,32 @@ const [doctors, setDoctors] = useState([]);
     console.log(doctors);
     return (
         <section id='prof' className='block prof-block'>
-            <Container fluid>
-                <div className='title-holder'>
+        <Container fluid >
+            <div className='title-holder'>
                 <h2>Nuestro Staff</h2>
                 <div className='subtitle'>Profesionales en constante capacitación para tu salud</div>
-                </div>
-                <Row>
-                {doctors?.map((doctor) => (
-                    <Col key={doctor.id} sm={4}>
+            </div>
+            <Row >
+                {doctors?.map((doctor, index) => (
+                <Col key={doctor.id} lg={3} md={3} sm={6} xs={12}>
+
                     <div className='holder'>
-                        <Card style={{ width: '18rem', textAlign: 'center' }}>
-                        <Card.Img variant='top' src={doctor.image} />
+                    <Card className='d-flex justify-content-center align-items-center'>
+                        <Card.Img variant='top' src='https://img.freepik.com/free-photo/healthcare-workers-medicine-covid-19-pandemic-self-quarantine-concept-smiling-attractive-doctor-scrubs-glasses-stethoscope-neck-cross-arms-chest-ready-help-patients_1258-58772.jpg?w=1480&t=st=1704003162~exp=1704003762~hmac=647e2eabf0ad156395b8cfb2ecfac29a686bfc1aaed4ca001d194601e1fccf49' />
                         <Card.Body>
-                            <Card.Title>{doctor.name +' ' +doctor.lastname}</Card.Title>
-                            <Card.Text>{doctor.specialities}</Card.Text>
-                            <Button variant='info'>Turnos disponibles</Button>
+                        <Card.Title>{doctor.name + ' ' + doctor.lastname}</Card.Title>
+                        <Card.Text>{doctor.specialities}</Card.Text>
+                        <Button variant='info'>Turnos disponibles</Button>
                         </Card.Body>
-                        </Card>
+                    </Card>
                     </div>
-                    </Col>
+                    {index === 3 && <div className='d-lg-none d-md-none d-sm-none' style={{ height: '30px' }}></div>}
+                    {/* Agrega un espacio vertical entre la cuarta y quinta tarjeta en pantallas pequeñas */}
+                </Col>
                 ))}
-                </Row>
+            </Row>
             </Container>
-            </section>
+        </section>
         );
     };
 
